4572b8856bd333992a2deb632f224d98a6c62851
jithin-space <jithinthankachan2600@gmail.com>
2017-05-25 17:06:48 +0530

    second submission

--- scripts/image-sequencing.js
+++ scripts/image-sequencing.js
@@ -16,175 +16,88 @@ H5P.ImageSequencing = (function(EventDispatcher, $, UI) {
     // Initialize event inheritance
     EventDispatcher.call(self);
 
-    var imageList = []; //list of images to be sequenced
-    var imageHolder = []; // list of imageholders
-    var level = 0; // game level ie number of images to sort
+    var elementList = []; //list of image
     var sequence = []; // getting a random sequence
-    var timer; // timer object for the game
-
-    /**
-     * get gameLevel
-     * @private
-     */
-    var getGameLevel = function() {
-      return imageList.length;
-    }
 
     /**
      * set gameLevel
+     * gameLevel is one more than the number of images
      * @private
      */
-
     var setGameLevel = function() {
-      level = imageList.length;
-    }
+      self.level = elementList.length;
+    };
+
 
     /**
-     * get the imageList
+     * creating the imageNodes for the linkedList
      * @private
      */
 
-    var getImageList = function() {
-      return imageList;
-    }
-
-    /**
-     * setting up the imageList from the parameters
-     * @private
-     */
-
-    var setImageList = function(images, id) {
+    var createImageNodes = function(images, id) {
       for (var i = 0; i < images.length; i++) {
-        var image = new ImageSequencing.Image(images[i], id, i, parameters.l10n.moves);
-        addImage(image);
+        var image = new ImageSequencing.Image(images[i].image, id, i, images[i].imageDescription);
+        image.on('drag', function() {
+          self.timer.play();
+        });
+        // registering event listeners on image object
+        image.on('dragstart', function(pos) {
+          self.handleDragStart(pos.data);
+        });
+        image.on('dragenter', function(pos) {
+          self.handleDragEnter(pos.data);
+        });
+        image.on('dragLeave', function(pos) {
+          self.handleDragLeave();
+        });
+        elementList.push(image); //push the created Node to the elementList
       }
 
-    }
+    };
 
     /**
-     * pushing H5P.imageSequencing.Image Objects to imageList
-     * attaching event listeners.
+     * creating the dropperNode for the linkedList
      * @private
      */
 
-    var addImage = function(image) {
-      image.on('reattach', function() {
-        reattach();
+    var createDropperNode = function(pos) {
+      var dropper = new ImageSequencing.Dropper(pos, parameters.l10n.dropperText);
+      // registering the event listeners
+      dropper.on('drop', function() {
+        self.handleDrop();
       });
-      image.on('drag', function() {
-        self.timer.play();
+      dropper.on('dragLeave', function() {
+        self.handleDragLeave();
       });
-      imageList.push(image);
-    }
+      elementList.push(dropper); //push the created Node to the elementList
+    };
 
     /**
-     *  instantiate an H5P.ImageSequencing.Dropper object with required pos
-     * attaching event listeners.
-     * @private
-     */
-    var setDropper = function() {
-      self.dropper = new ImageSequencing.Dropper();
-      self.dropper.on('reattach', function() {
-        reattach();
-      })
-    }
-
-    /**
-     * get the dropper for the game
-     * @private
-     */
-    var getDropper = function() {
-      return self.dropper;
-    }
-
-    /**
-     * generate a random sequence between [0 to level-1]
-     * to shuffle the imageList on every page reload
+     * generate a random sequence between [0 to gameLevel-1]
+     * to shuffle the elementList on every page reload
      * @private
      */
     var setRandomSequence = function() {
       sequence = Array.apply(null, {
-        length: level
-      }).map(Number.call, Number);
-      H5P.shuffleArray(sequence);
+        length: self.level
+      }).map(Number.call, Number); // array[0..N-1]
+      H5P.shuffleArray(sequence); // shuffle the array
+    };
 
-    }
     /**
-     * set the postion of images on imageList for the first time
-     * using the random sequence generated
+     * give each element in the elementList a random position
+     * called only once during the initial game loading
      * @private
      */
-    var setImagePos = function() {
-      for (var i = 0; i < level; i++) {
-        imageList[i].setPos(sequence[i]);
+    var setListPos = function() {
+      for (var i = 0; i < elementList.length; i++) {
+        elementList[i].setPos(sequence[i]);
       }
-    }
-    /**
-     * setting the previous and next links to all
-     * H5P.ImageSequencing.Image Objects on the imageList
-     * @private
-     */
-    var setLinks = function() {
-      imageList.sort(reorder);
-      for (var i = 0; i < level; i++) {
-        imageList[i].setNext(imageList[i + 1]);
-        imageList[i].setPrev(imageList[i - 1]);
-      }
-    }
-
-    var setImageHolder = function() {
-      for (var i = 0; i < level; i++) {
-        position = imageList[i].getPos();
-        if (position >= 0) {
-          imageHolder[position] = $('<div class="imageHolder"></div>');
-          imageList[i].appendTo(imageHolder[position], self.size);
-        } else {
-          position = position - (position * 2); //negative pos means need to dropper object in that pos
-          imageHolder[position] = $('<div class="imageHolder"></div>');
-          self.dropper.setPos(position); //set the position of the dropper to current position
-          self.dropper.appendTo(imageHolder[position], self.size);
-        }
-
-      }
-    }
-
-    var scaleGameSize = function() {
-      // resizing value;
-      // console.log(self.$wrapper.width());
-      self.size = calculate(self.$wrapper.width());
-      reattach();
-    }
+      elementList.sort(reorder); //reorder the elements on the list based on their current positions
+    };
 
     /**
-     * function to initalize game by setting all game parameters
-     * @private
-     */
-
-    var initializeGame = function(images, id) {
-      setImageList(images, id); //initialize imageList
-      setDropper();
-      setGameLevel(); //initialize gameLevel
-      setRandomSequence(); //create a random sequence
-      // imageList.sort(reorder);
-      setImagePos(); //set image positions based on the random sequence
-      setLinks(); // set previous and next links to each image elements
-    }
-
-    /**
-     * calculate the holderSize and imageSize
-     * according to the wrapper width
-     * @private
-     */
-
-
-    var calculate = function(width) {
-      var holderSize = (width - (2 * level)) / level;
-      var imageSize = (holderSize - (20));
-      return [holderSize, imageSize];
-    }
-    /**
-     * sorting imageList according to the pos values
-     * required for proper link setting
+     * sorting elementList according to the pos values
      * @private
      */
     var reorder = function(a, b) {
@@ -193,27 +106,92 @@ H5P.ImageSequencing = (function(EventDispatcher, $, UI) {
       if (a.getPos() > b.getPos())
         return 1;
       return 0;
-    }
+    };
 
+    /**
+     * creating the Nodes for the linkedList
+     * nodes include both Image and Dropper Nodes
+     * @private
+     */
+    var createHolderNodes = function(images, id) {
+      createImageNodes(images, id);
+      createDropperNode();
+    };
+    /**
+     * initialize a linkedList for the game
+     * @private
+     */
+    var initializeLinkedList = function() {
+      self.linkedList = new ImageSequencing.LinkedList();
+    };
+    /**
+     * add the created Nodes in the elementList to the linkedList
+     * @private
+     */
+    var addNodesToList = function() {
+      for (var i = 0; i < elementList.length; i++) {
+        self.linkedList.add(elementList[i]);
+      }
+    };
+
+    /**
+     * function to initalize game by setting all game parameters
+     * @private
+     */
+
+    var initializeGame = function(images, id) {
+      createHolderNodes(images, id);
+      initializeLinkedList();
+      setGameLevel();
+      setRandomSequence();
+      setListPos();
+      addNodesToList();
+    };
+
+    /**
+     * function to resize the elements according to the current window size
+     * triggered by the resize event
+     * @private
+     */
+
+    var scaleGameSize = function() {
+      self.size = calculate(self.$wrapper.width(), self.level);
+      if (self.isSubmitted) {
+        gameSubmitted();
+      } else {
+        self.reattach();
+      }
+    };
+
+    /**
+     * calculate the holderSize and imageSize
+     * according to the wrapper width and no. of elements to display
+     * @private
+     */
+    var calculate = function(width, level) {
+      var holderSize = (width - (4 * level)) / level;
+      var imageSize = (holderSize - (20));
+      return [holderSize, imageSize];
+    };
 
     /**
      * calculate the score each time user submits
      * @private
      */
-    var calculateScore = function() {
-      score = 0
-      imageList.forEach(function(element) {
-        if (element.getPos() == element.getSequenceNo()) {
+    var calculateScore = function(elementList) {
+      var score = 0;
+      for (var i = 0; i < elementList.length; i++) {
+        if (i == elementList[i].getSequenceNo()) {
           score++;
-          element.setCorrect();
+          elementList[i].setCorrect();
         } else {
-          element.setIncorrect();
+          elementList[i].setIncorrect();
         }
-      });
-      self.$progressBar.setScore(score);
-      feedbackMessage = getFeedbackMessage((score / level) * 100, parameters.l10n.feedbacks); //get the feedback message based on the performance
+      }
+      self.$progressBar.setScore(score); //set the score on the progressBar
+      var feedbackMessage = getFeedbackMessage((score / (self.level - 1)) * 100, parameters.l10n.feedbacks); //get the feedback message based on the performance
       self.$feedback.html(feedbackMessage); //set the feedback to feedbackMessage obtained.
-    }
+    };
 
     /**
      * get the feedbackMessage based on the performance
@@ -221,7 +199,7 @@ H5P.ImageSequencing = (function(EventDispatcher, $, UI) {
      */
 
     var getFeedbackMessage = function(performance, feedbacks) {
-
+      var msg = "";
       switch (true) {
         case (performance < 25):
           msg = feedbacks.poorEffort;
@@ -239,36 +217,71 @@ H5P.ImageSequencing = (function(EventDispatcher, $, UI) {
           msg = feedbacks.successFeedback;
       }
       return msg;
-    }
+    };
 
+    /**
+     * displaySubmittedList after eliminating the dropper element
+     * @private
+     */
+
+    var displaySubmission = function(submittedList) {
+      self.$wrapper.empty(); //empty the whole wrapper
+      self.$dragZone.empty(); //unset the dragZone component
+      var size = calculate(self.$wrapper.width(), self.level - 1);
+      self.$dragZone = $('<div class="sequencing-grid"/>'); //reinitiate dragZone
+      for (var i = 0; i < submittedList.length; i++) {
+        submittedList[i].appendTo(self.$dragZone, size);
+      }
+      self.attachRest();
+    };
+
+    /**
+     * remove the dropper element from the submittedList and
+     * pass that trimmed list to the displaySubmittedList function
+     * @private
+     */
+
+    var prepareSubmission = function() {
+      var submittedList = [];
+      for (var i = 0; i < self.level; i++) {
+        if (!(self.linkedList.getNodeAt(i).getType() == "Dropper")) {
+          submittedList.push(self.linkedList.getNodeAt(i));
+        }
+      }
+      displaySubmission(submittedList);
+      calculateScore(submittedList);
+    };
     /**
      * trigger when the user submits the game
      * @private
      */
-
     var gameSubmitted = function() {
-      calculateScore(); //calculateScore based on the current positioning
+      //set for getting the current status of the game ie submitted or not
+      self.isSubmitted = true;
+      prepareSubmission();
       self.timer.stop(); //stop the timer
-      self.counter.increment(); // increment the number of submissions
-      self.$feedback.addClass('h5p-show'); //show  feedbackMessage
-      self.$progressBar.$scoreBar.addClass('h5p-show'); //show progressBar
-    }
+      self.$feedback.addClass('sequencing-feedback-show'); //show  feedbackMessage
+      self.$progressBar.$scoreBar.addClass('sequencing-feedback-show'); //show progressBar
+    };
 
     /**
-     * trigger when ever a shift in positioning occurs
+     * trigger when ever a shift in position occurs
      * during both drag and drop
      * @private
      */
-    var reattach = function() {
+    self.reattach = function() {
       self.$wrapper.empty(); //empty the whole wrapper
       self.$dragZone.empty(); //unset the dragZone component
-      setLinks(); //reset the links
-      self.$dragZone = $('<div id="columns-full" class="l-col-grid"/>'); //reinitiate dragZone
-      setImageHolder(); // reinitiate imageHolders
-      for (var i = 0; i < level; i++) {
-        imageHolder[i].appendTo(self.$dragZone, self.size); //append each to dragZone
+      self.$dragZone = $('<div  class="sequencing-grid"/>'); //reinitiate dragZone
+      for (var i = 0; i < self.level; i++) {
+        self.linkedList.getNodeAt(i).appendTo(self.$dragZone, self.size);
       }
-      if (level) {
+      self.attachRest();
+    };
+
+    /* for attaching components other than the dragZone */
+    self.attachRest = function() {
+      if (self.level) {
         self.$dragZone.appendTo(self.$wrapper);
         self.$feedback.appendTo(self.$wrapper);
         self.$progressBar.appendTo(self.$wrapper);
@@ -282,7 +295,7 @@ H5P.ImageSequencing = (function(EventDispatcher, $, UI) {
         });
         self.$submit.appendTo(self.$wrapper);
       }
-    }
+    };
 
     /*
      * initalizeGame...
@@ -291,28 +304,60 @@ H5P.ImageSequencing = (function(EventDispatcher, $, UI) {
      */
     initializeGame(parameters.sequenceimages, id);
 
+    /* event listeners triggerd either by image nodes or dropper node
+     * for overall rearrangement in the LinkedList
+     */
+
+    /*
+     * continue handling the dragstart event started by the anyone of the image nodes
+     */
+    self.handleDragStart = function(pos) {
+      self.dragSrcEl_ = self.linkedList.getNodeAt(pos);
+      self.isSubmitted = false;
+    };
+    /*
+     * continue handling the dragenter event triggerd by either an image or dropper node
+     */
+    self.handleDragEnter = function(pos) {
+      self.dragUnderEl_ = self.linkedList.getNodeAt(pos);
+      if (self.linkedList.getDropperPos() != self.dragUnderEl_.getPos() && self.dragUnderEl_.getPos() != self.dragSrcEl_.getPos()) {
+        self.linkedList.moveThroughList(self.dragUnderEl_.getPos(), self.linkedList.getDropperPos());
+        self.reattach();
+      }
+      self.dragSrcEl_.$dropper.addClass('moving');
+    };
+    self.handleDragLeave = function() {
+      self.dragSrcEl_.$dropper.removeClass('moving');
+    };
+
+    self.handleDrop = function() {
+      var dropperPos = self.linkedList.getDropperPos();
+      self.linkedList.moveThroughList(self.dragSrcEl_.getPos(), self.linkedList.getDropperPos()); //move dropper to dragSrcEl_'s position
+      self.linkedList.moveThroughList(dropperPos, self.dragSrcEl_.getPos()); //move dragSrcEl_ to the dropper's previous position
+      self.reattach();
+      self.counter.increment(); //increment the counter for each movement occurs
+    };
     /**
      * Attach this game's html to the given container.
      *
      * @param {H5P.jQuery} $container
      */
     self.attach = function($container) {
-      self.$wrapper = $container.addClass('l-wrap').html('');
-      self.size = calculate(self.$wrapper.width());
-      self.$dragZone = $('<div id="columns-full" class="l-col-grid"/>');
-      setImageHolder();
-      for (var i = 0; i < level; i++) {
-        imageHolder[i].appendTo(self.$dragZone, self.size);
+      self.$wrapper = $container.addClass('sequencing-wrapper').html('');
+      self.size = calculate(self.$wrapper.width(), self.level);
+      self.$dragZone = $('<div class="sequencing-grid"/>');
+      for (var i = 0; i < self.level; i++) {
+        self.linkedList.getNodeAt(i).appendTo(self.$dragZone, self.size);
       }
-      if (level) {
+      if (self.level) {
         self.$dragZone.appendTo($container);
-        self.$feedback = $('<div class="h5p-feedback"></div>');
-        self.$progressBar = UI.createScoreBar(level, 'scoreBarLabel');
+        self.$feedback = $('<div class="sequencing-feedback"></div>');
+        self.$progressBar = UI.createScoreBar(self.level - 1, 'scoreBarLabel');
         self.$progressBar.appendTo($container);
-        self.$progressBar.$scoreBar.addClass("h5p-feedback");
+        self.$progressBar.$scoreBar.addClass("sequencing-feedback");
         self.$feedback.appendTo($container);
-        self.$status = $('<dl class="h5p-status">' + '<dt>' + parameters.l10n.timeSpent + '</dt>' + '<dd class="h5p-time-spent">0:00</dd>' +
-          '<dt>' + parameters.l10n.totalSubmits + '</dt>' + '<dd class="h5p-submits">0</dd>' + '</dl>');
+        self.$status = $('<dl class="sequencing-status">' + '<dt>' + parameters.l10n.timeSpent + '</dt>' + '<dd class="h5p-time-spent">0:00</dd>' +
+          '<dt>' + parameters.l10n.totalMoves + '</dt>' + '<dd class="h5p-submits">0</dd>' + '</dl>');
         self.$status.appendTo($container);
         self.$submit = UI.createButton({
           title: 'Submit',
@@ -325,13 +370,13 @@ H5P.ImageSequencing = (function(EventDispatcher, $, UI) {
         self.timer = new ImageSequencing.Timer(self.$status.find('.h5p-time-spent')[0]);
         self.counter = new ImageSequencing.Counter(self.$status.find('.h5p-submits'));
       }
-    }
-
-    if (level) {
+    };
+    // registering the H5P.ImageSequencingObject to handle the resize event
+    if (self.level) {
       self.on('resize', scaleGameSize);
     }
 
-  };
+  }
   // Extends the event dispatcher
   ImageSequencing.prototype = Object.create(EventDispatcher.prototype);
   ImageSequencing.prototype.constructor = ImageSequencing;
--- scripts/image.js
+++ scripts/image.js
@@ -7,60 +7,37 @@
    * @extends H5P.EventDispatcher
    * @param {Object} image
    * @param {number} id
-   * @param {number} seq_no
-   * @param {string} [description]
+   * @param {number} seqNumber
+   * @param {string} imageDescription
    */
-  ImageSequencing.Image = function(image, id, seqNumber, movesText) {
+  ImageSequencing.Image = function(image, id, seqNumber, imageDescription) {
     /** @alias H5P.ImageSequencing.Image# */
     var self = this;
     // Initialize event inheritance
     EventDispatcher.call(self);
-
     var path = H5P.getPath(image.path, id);
     var seqNo = seqNumber;
     var description = description;
-    var moves = 0;
-
     /**
      * get the image description
      * @returns {string}
      */
-    self.getMovesText = function() {
-      return movesText;
+    self.getImageDescription = function() {
+      return imageDescription;
     };
-
     /**
      * set the image position
      */
-
     self.setPos = function(pos) {
       self.pos = pos;
-    }
-
-    /**
-     * get the number of image moves
-     * @returns {number}
-     */
-
-    self.getMoves = function() {
-      return moves;
-    }
-
-    /**
-     * increment the image moves
-     */
-
-    self.setMoves = function() {
-      moves++;
-    }
-
+    };
     /**
      * get the image position
      * @returns {number}
      */
     self.getPos = function() {
       return self.pos;
-    }
+    };
     /**
      * get the image sequence number
      * @returns {number}
@@ -69,106 +46,74 @@
       return seqNo;
     };
     /**
-     * set it to the class correct*/
+     * set it to the class correct
+     */
     self.setCorrect = function() {
-      self.$dropper.removeClass('incorrect');
-      self.$dropper.addClass('correct');
-    }
+      self.$dropper.removeClass('sequencing-incorrect');
+      self.$dropper.addClass('sequencing-correct');
+    };
     /**
-     * set it to the class incorrect*/
+     * set it to the class incorrect
+     */
     self.setIncorrect = function() {
-      self.$dropper.removeClass('correct');
-      self.$dropper.addClass('incorrect');
-    }
+      self.$dropper.removeClass('sequencing-correct');
+      self.$dropper.addClass('sequencing-incorrect');
+    };
     /**
-     * set next to the next element in the imageList
-     * @param {H5P.ImageSequencing.Image} image
+     * set next to the next node in the linkedList
+     * @param {H5P.ImageSequencing.Image} or {H5P.ImageSequencing.Dropper} node
      */
-    self.setNext = function(image) {
-      self.next = image;
-    }
+    self.setNext = function(node) {
+      self.next = node;
+    };
 
     /**
-     * set previous to previous element in the imageList
-     * @param {H5P.ImageSequencing.Image} image
+     * set previous to previous node in the linkedList
+     * @param {H5P.ImageSequencing.Image} or {H5P.ImageSequencing.Dropper} node
      */
-    self.setPrev = function(image) {
-      self.prev = image;
-    }
-
+    self.setPrev = function(node) {
+      self.prev = node;
+    };
+    /*
+     *for getting the type of the node in the linked list
+     * @return {String}
+     */
+    self.getType = function() {
+      return "Image";
+    };
     /**
-     * get the next element in the imageList
-     * @return {H5P.ImageSequencing.Image}
+     * get the next element in the linkedList
+     * @return {H5P.ImageSequencing.Image} or {H5P.ImageSequencing.Dropper}
      */
     self.getNext = function() {
       return self.next;
-    }
+    };
 
     /**
      * get the previous element in the imageList
      * @return {H5P.ImageSequencing.Image}
      */
-    self.getPrevious = function() {
+    self.getPrev = function() {
       return self.prev;
-    }
-
-    /**
-     * give correctly positioned element a green border
-     */
-    self.setCorrect = function() {
-      self.$dropper.css('border', '2px solid green');
-    }
-
-    /**
-     * give incorrectly positioned element a red border
-     */
-    self.incorrect = function() {
-      self.$dropper.css('border', '2px solid red');
-    }
+    };
 
     /**
      * when dragstart event is fired on the image
      */
     self.handleStart = function(e) {
       self.$dropper.css('opacity', '0.4');
-      dragSrcEl_ = self;
       self.$dropper.addClass('moving');
       e.originalEvent.dataTransfer.effectAllowed = 'move';
       self.trigger('drag'); //for firing the timer when drag starts on an image element
-    }
+      self.trigger('dragstart', self.pos); //for firing the dragStart handling at the linkedList
+    };
     /**
      * when an image (drag) enter into the droppable (self) area of another image
      */
     self.handleDragEnter = function(e) {
       self.$dropper.addClass('over');
-      if (dragSrcEl_.pos != self.pos) {
-        var i = self.pos;
-        var move = Math.abs(dragSrcEl_.pos) - self.pos;
-        var current = self;
-        if (move > 0) {
-          //shift all elements to right starting from the current position
-          while (current != undefined && move != 0) {
-            next = current.getNext();
-            current.pos += 1;
-            current = next;
-            move--;
-          }
-        } else {
-          //shift all elements to left starting from current position
-          while (current != undefined && move != 0) {
-            prev = current.getPrevious();
-            current.pos -= 1;
-            current = prev;
-            move++;
-          }
-        }
-
-        dragSrcEl_.pos = i - (i * 2); //set its pos negative so that a dropper can be placed at that position
-        self.trigger('reattach'); //trigger reattach of the images to the container
-
-      }
-
-    }
+      self.trigger('dragenter', self.pos); //for firing dragenter event handling at linkeList
+    };
 
     /**
      * when an image(self) enter into the droppable area of another image
@@ -179,25 +124,25 @@
       }
       e.originalEvent.dataTransfer.dropEffect = 'move';
       return false;
-    }
+    };
 
     /**
      * when an image leaves the droppable area of another image (self)
      */
     self.handleDragLeave = function(e) {
       self.$dropper.removeClass('over');
-    }
+      self.trigger('dragLeave'); //for firing the dragleave event handling at the linkedList
+    };
     /**
      * when an image dragging finally ends.
      */
 
     self.handleDragEnd = function(e) {
       self.$dropper.removeClass('moving');
-      self.$dropper.css('opacity', '1');
-    }
+    };
 
     /**
-     * when an image leaves the droppable area of another image (self)
+     * when an image leaves the droppable area of another node (self)
      */
     self.handleDrop = function(e) {
 
@@ -205,8 +150,9 @@
         e.preventDefault(); // stops the browser from redirecting.
       }
       self.$dropper.removeClass('over');
+      self.$dropper.removeClass('moving');
       return false;
-    }
+    };
 
     /**
      * Append the image to the imageHolder
@@ -216,27 +162,29 @@
      * @param {array} size
      */
 
-
     self.appendTo = function($container, size) {
-      self.$dropper = $('<div class="columns" width="' + size[0] + 'px" height="' + size[0] + 'px" draggable="true"><header class="count" data-col-moves="0">' + self.getMovesText() + ':' + self.getMoves() + '</header><div>\
-            <img src="' + path + '"  alt="Sequence Image Card" width="' + size[1] + 'px" height="' + size[1] + 'px"/></div></div>').appendTo($container);
+      self.$dropper = $('<div class="sequencing-columns" width="' + size[0] + 'px" height="' + size[0] + 'px" draggable="true">\
+                            <header class="sequencing-header" >' + self.getImageDescription() + '</header><div>\
+                              <img src="' + path + '"  alt="Sequence Image Card" width="' + size[1] + 'px" height="' + size[1] + 'px"/>\
+                            </div></div>').appendTo($container);
+      // registering event listeners
       self.$dropper.on('dragstart', function(e) {
-        self.handleStart(e)
+        self.handleStart(e);
       });
       self.$dropper.on('dragenter', function(e) {
-        self.handleDragEnter(e)
+        self.handleDragEnter(e);
       });
       self.$dropper.on('dragover', function(e) {
-        self.handleDragOver(e)
+        self.handleDragOver(e);
       });
       self.$dropper.on('dragleave', function(e) {
-        self.handleDragLeave(e)
+        self.handleDragLeave(e);
       });
       self.$dropper.on('dragend', function(e) {
-        self.handleDragEnd(e)
+        self.handleDragEnd(e);
       });
       self.$dropper.on('drop', function(e) {
-        self.handleDrop(e)
+        self.handleDrop(e);
       });
     };
   };
