1a811fd3cc58cf52b4e41d48dcd5f3159eee3896
Thomas Horn Sivertsen <thomass@cerpus.com>
2018-03-28 11:44:54 +0200

    Added JSDoc

--- presave.js
+++ presave.js
@@ -1,5 +1,12 @@
 var H5PPresave = H5PPresave || {};
 
+/**
+ * Resolve the presave logic for the content type Image Sequencing
+ *
+ * @param {object} content
+ * @param finished
+ * @constructor
+ */
 H5PPresave['H5P.ImageSequencing'] = function (content, finished) {
   var presave = H5PEditor.Presave;
 
@@ -15,6 +22,10 @@ H5PPresave['H5P.ImageSequencing'] = function (content, finished) {
     finished({maxScore: score});
   }
 
+  /**
+   * Check if required parameters is present
+   * @return {boolean}
+   */
   function isContentInValid() {
     return !presave.checkNestedRequirements(content, 'content.sequenceImages') || !Array.isArray(content.sequenceImages);
   }
